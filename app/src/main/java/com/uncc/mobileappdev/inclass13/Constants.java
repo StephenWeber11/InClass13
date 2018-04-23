5602937be30497be04142a4112dc5dbf4f32878c
Stephen Weber <stephen.weber3@gmail.com>
2018-04-23 18:55:52 -0400

    Initial commit

---
+++ app/src/main/java/com/uncc/mobileappdev/inclass13/Constants.java
@@ -0,0 +1,10 @@
+package com.uncc.mobileappdev.inclass13;
+
+/**
+ * Created by Stephen on 4/23/2018.
+ */
+
+public class Constants {
+    public static final String APP_TITLE = "MessageMe!";
+    public static final String APP_TITLE_SIGNUP = "MessageMe! (Signup)";
+}
