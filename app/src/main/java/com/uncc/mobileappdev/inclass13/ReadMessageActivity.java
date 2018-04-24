38b0ddd28dde1c48526df26fff35d4524278ad8a
Stephen Weber <stephen.weber3@gmail.com>
2018-04-24 19:16:58 -0400

    Create recyclerview and layout to inflate for inbox

---
+++ app/src/main/java/com/uncc/mobileappdev/inclass13/ReadMessageActivity.java
@@ -0,0 +1,13 @@
+package com.uncc.mobileappdev.inclass13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_message);
+    }
+}
